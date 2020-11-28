15de7fb8ae197d4d9559f3ae0cf937d7ff55d909
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-11-28 10:27:06 +0530

    Backend improved
    Backend improved

--- CustomUser/CustomUser/settings.py
+++ CustomUser/CustomUser/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
@@ -122,3 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
--- CustomUser/UserApp/views.py
+++ CustomUser/UserApp/views.py
@@ -1,36 +1,56 @@
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
 def home(request):
     return render(request,"pages/home.html")
 
+@csrf_exempt
 def create_superuser(request):
     if request.method == "POST":
         username1 = request.POST['username1']
         email1 = request.POST['email1']
         password1 = request.POST['password1']
-        print(username1,email1,password1)
+        user = User.objects.create_user(username1,email1,password1)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        messages.success(request,"SuperUser Created Successfully")
         return redirect('/')
     else:
         return HttpResponse("<h1>404 - Not Found</h1>")
 
+@csrf_exempt
 def create_adminuser(request):
     if request.method == "POST":
         username2 = request.POST['username2']
         email2 = request.POST['email2']
         password2 = request.POST['password2']
-        print(username2,email2,password2)
+        user = User.objects.create_user(username2,email2,password2)
+        user.is_superuser = True
+        user.save()
+        messages.success(request,"AdminUser Created Successfully")
         return redirect('/')
     else:
         return HttpResponse("<h1>404 - Not Found</h1>")
 
+@csrf_exempt
 def create_normaluser(request):
     if request.method == "POST":
         username3 = request.POST['username3']
         email3 = request.POST['email3']
         password3 = request.POST['password3']
-        print(username3,email3,password3)
+        User.objects.create_user(username3,email3,password3).save()
+        messages.success(request,"NormalUser Created Successfully")
         return redirect('/')
     else:
         return HttpResponse("<h1>404 - Not Found</h1>")
+
+
+
+
+# user  superuser
+# email  superuser@gmail.com
+# pass  superuser
